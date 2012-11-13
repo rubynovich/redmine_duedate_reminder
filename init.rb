5ac49dcb21c2d16ca1af34a1b2416b1c9cadb93c
Rubynovich <roman@shipiev.me>
2012-11-13 13:54:01 +0400

    fix перешел на русский язык в описании и README

--- init.rb
+++ init.rb
@@ -1,10 +1,10 @@
 require 'redmine'
 
 Redmine::Plugin.register :redmine_duedate_reminder do
-  name 'Duedate reminder'
+  name 'Напоминалка о просроченных задачах'
   author 'Roman Shipiev'
-  description 'E-mail notification of issues due date you are involved in (Assignee, Author, Watcher)'
-  version '0.0.3'
+  description 'Рассылает напоминания посредством E-Mail, что срок выполнения задачи уже близок. Рассылка производится как исполнителям, так и авторам с наблюдателями. Модуль реализован в виде rake-задачи'
+  version '0.0.4'
   url 'https://github.com/rubynovich/redmine_duedate_reminder'
   author_url 'http://roman.shipiev.me'
 end
